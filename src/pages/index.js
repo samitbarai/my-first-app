1bf3d790e7aba944af1cff5f969bd84802e26368
samitbarai <samit.barai@juspay.in>
2018-07-27 12:54:47 +0530

    Added CSS grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>First Attempt to Design in React</h1>
-    <p>This is my first take on a React website looking at Design+Code tutorial</p>
-    <p>This is just another paragraph to play around this thing</p>
-    <Link to="/page-2/">Know more in this about this project</Link>
+    <div className = "Hero">
+      <div className = "HeroGroup">
+        <h1>First Attempt to Design in React</h1>
+        <p>This is my first take on a React website looking at Design+Code tutorial</p>
+        <p>This is just another paragraph to play around this thing</p>
+        <Link to="/page-2/">Know more</Link>
+      </div>
+    </div>
+
   </div>
 )
 
