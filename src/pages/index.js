44a7b88706d91eff5ee8b66262b5dccc1ef3a412
samitbarai <samit.barai@juspay.in>
2018-07-27 22:58:29 +0530

    Chapter 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card'
 
 const IndexPage = () => (
   <div>
     <div className = "Hero">
       <div className = "HeroGroup">
-        <h1>First Attempt to Design in React</h1>
+        <h1>First Attempt <br /> to Design in React</h1>
         <p>This is my first take on a React website looking at Design+Code tutorial</p>
-        <p>This is just another paragraph to play around this thing</p>
         <Link to="/page-2/">Know more</Link>
       </div>
     </div>
-
+    <div className = "Cards">
+      <h2> 11 more courses left </h2>
+      <div className = "CardGroup" >
+      <Card
+        title = "React for Samit"
+        text = "12 sections"
+        image = {require('../images/wallpaper.jpg')} 
+      />
+      <Card
+        title = "React for Samit"
+        text = "12 sections"
+        image = {require('../images/wallpaper.jpg')} 
+      />
+      <Card
+        title = "React for Samit"
+        text = "12 sections"
+        image = {require('../images/wallpaper.jpg')} 
+      />
+      <Card
+        title = "React for Samit"
+        text = "12 sections"
+        image = {require('../images/wallpaper.jpg')} 
+      />
+      </div>
+    </div>
   </div>
 )
 
